942483215b9fd93d11ced7e2032d38c191ff570f
Emmett Lee <emmetlee@avaya.com>
2019-11-28 23:24:10 +0000

    Updated solution_f25ffba3.py with code to print out soln grids

--- src/solution_f25ffba3.py
+++ src/solution_f25ffba3.py
@@ -19,37 +19,36 @@ def solve(grid):
     #Open the json file passed into the solve function
     d1 = json.load (open(grid))
 
-#    tmpd = {k: len(v) for k,v in d.items()}
-#    lengths = [lengthv for lengthv in tmpd.values()]
+    tmpd1 = {k: len(v) for k,v in d1.items()}
+    lengths1 = [lengthv for lengthv in tmpd1.values()]
     
     """Print the Output Grids for training inputs"""
-    
-#    for i in range(lengths[1]):
-#        for j in range(len(d['train'][0]['input'][i])):
-#            output_grid1 = []
-#            output_grid1.extend(d['train'][i]['input'][j])
-#            for z in range(len(output_grid1)):
-#                output_grid1.append(output_grid1[2-z])
-#            print(output_grid1)
-#        print(" ")    
+    #training grids are in lengths[0]
+    for x in range(lengths1[0]):
+        for y in reversed(range(len(d1['train'][0]['input']))):
+            output_grid2 = []
+            if y>=5:
+                output_grid2.extend(d1['train'][x]['input'][y])
+            else:
+                output_grid2.extend((d1['train'][x]['input'][-y+9]))
+            print(output_grid2)
+        print(" ")    
 
     """Print the Output Grids for Evaluation inputs"""
     
-#    for i in range(lengths[0]):
-#        for j in range(len(d['test'][0]['input'][i])):
-#            output_grid2 = []
-#            output_grid2.extend(d['test'][i]['input'][j])
-#            for z in range(len(output_grid2)):
-#                output_grid2.append(output_grid2[2-z])
-#            print(output_grid2)
-    
-    #return(output_grid2)
-
+    for x in range(lengths1[1]):
+        for y in reversed(range(len(d1['test'][0]['input']))):
+            output_grid2 = []
+            if y>=5:
+                output_grid2.extend(d1['test'][x]['input'][y])
+            else:
+                output_grid2.extend((d1['test'][x]['input'][-y+9]))
+            print(output_grid2)
+        print(" ")
 
 def main():
     """Call solve() function and pass the json"""
     output = solve("f25ffba3.json")
     
     
-  
 main()
